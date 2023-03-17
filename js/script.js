97c6e4aecbd048a89224b48a8b15516853ae6edc
lost <111534592+lost0n@users.noreply.github.com>
2023-03-17 15:34:26 -0300

    Novo Commit
    Corrigindo bugs.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+var push = document.querySelector('#pushclass')
 
+function abrirmenu() {
+  if (push.classList.contains('show') == true) {
+    push.removeAttribute('class', 'show')
+  } else {
+    push.setAttribute('class', 'show')
+  }
+}
