5544de18ffea87835a4983581f82f18b7599216f
lost <111534592+lost0n@users.noreply.github.com>
2023-03-28 17:23:57 -0300

    New Commit
    Removed Bugs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 var push = document.querySelector('#pushclass')
+var menu = document.querySelector('.simbolo')
 
 function abrirmenu() {
   if (push.classList.contains('show') == true) {
     push.removeAttribute('class', 'show')
+    menu.innerHTML = ' close '
   } else {
     push.setAttribute('class', 'show')
+    menu.innerHTML = ' menu '
   }
 }
